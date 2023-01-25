ec53de0d50c319a2c641c982e7c7385ee66d5aa0
toerso <toerso.mechanix@gmail.com>
2023-01-25 19:23:43 +0600

    boot event created

--- src/Artificers/Foundation/Events/BootEvent.php
+++ src/Artificers/Foundation/Events/BootEvent.php
@@ -6,10 +6,14 @@ use Artificers\Events\Event;
 use Artificers\Foundation\Rayxsi;
 
 class BootEvent extends Event {
-    public const type = 'boot';
-    public Rayxsi $rXsiApp;
+    public const type = 'booting';
+    private Rayxsi $rXsiApp;
 
     public function __construct(Rayxsi $rXsiApp) {
         $this->rXsiApp = $rXsiApp;
+    }
+
+    public function getRayxsi(): Rayxsi {
+        return $this->rXsiApp;
     }
 }
--- src/Artificers/Foundation/Events/PreBootEvent.php
+++ src/Artificers/Foundation/Events/PreBootEvent.php
@@ -2,7 +2,14 @@
 
 namespace Artificers\Foundation\Events;
 
-class PreBootEvent
-{
+use Artificers\Events\Event;
+use Artificers\Foundation\Rayxsi;
 
+class PreBootEvent extends Event {
+    public const type = 'boot_processing';
+    protected Rayxsi $rayxsi;
+
+    public function __construct(Rayxsi $rayxsi) {
+        $this->rayxsi = $rayxsi;
+    }
 }
