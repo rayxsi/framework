f9b6e0ee6d0a97bb615086060257c86bf3d5175e
toerso <toerso.mechanix@gmail.com>
2023-01-25 19:27:24 +0600

    added app version controll middleware

--- src/Artificers/Foundation/Middleware/VersionChecker.php
+++ src/Artificers/Foundation/Middleware/VersionChecker.php
@@ -1,8 +1,15 @@
 <?php
-
+declare(strict_types=1);
 namespace Artificers\Foundation\Middleware;
 
-class VersionChecker
-{
+use Artificers\Foundation\Rayxsi;
+use Closure;
 
+class VersionChecker {
+    public function __invoke(Rayxsi $rayxsi, Closure $next) {
+        if(version_compare($phpVersion = PHP_VERSION, $rayxsiMinVersion = $rayxsi::$minimumPhpVersion, '<')) {
+            die(sprintf('You are currently running PHP version[%s], but the Rayxsi framework requires at least PHP version [%s]', $phpVersion, $rayxsiMinVersion));
+        }
+        return $next($rayxsi);
+    }
 }
